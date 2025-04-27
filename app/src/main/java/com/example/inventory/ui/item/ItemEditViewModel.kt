0c1b7200eb746efb00e57e2376a8981d37359860
SaraDoesIt <sclark@foghornconsulting.com>
2025-04-27 13:19:23 -0400

    Add edit functionality that persists updated data.

--- app/src/main/java/com/example/inventory/ui/item/ItemEditViewModel.kt
+++ app/src/main/java/com/example/inventory/ui/item/ItemEditViewModel.kt
@@ -52,6 +52,18 @@ class ItemEditViewModel(
         }
     }
 
+    fun updateUiState(itemDetails: ItemDetails) {
+        itemUiState =
+            ItemUiState(itemDetails = itemDetails, isEntryValid = validateInput(itemDetails))
+    }
+
+    // Save the updated entity to the Room database
+    suspend fun updateItem() {
+        if (validateInput(itemUiState.itemDetails)) {
+            itemsRepository.updateItem(itemUiState.itemDetails.toItem())
+        }
+    }
+
     private fun validateInput(uiState: ItemDetails = itemUiState.itemDetails): Boolean {
         return with(uiState) {
             name.isNotBlank() && price.isNotBlank() && quantity.isNotBlank()
